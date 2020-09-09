de6be3b373351b2c1c0264a65ae4b01286a245fa
Matt Pryor <mapryor1991@yahoo.com>
2020-09-09 10:27:49 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,1 @@
-sudo apt install ansible -y && git clone https://github.com/mpryor/ansible-playbooks
+sudo apt install ansible -y && sudo apt install git -y && git clone https://github.com/mpryor/ansible-playbooks
